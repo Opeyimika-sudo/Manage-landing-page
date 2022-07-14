5d8437a1e1eb284cd2b02bb1c9ffcf1f81f672fc
Opeyimika-sudo <54380080+Opeyimika-sudo@users.noreply.github.com>
2022-07-14 03:06:52 +0100

    Pushed javascript required for some animation

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Declaration of variables for navbar
+
 const hamburger = document.querySelector('.header--hamburger');
 const closehamburger = document.querySelector('.header--close');
 const header = document.querySelector('.header');
@@ -5,6 +7,7 @@ const navbar = document.querySelector('.nav');
 const navbarli = document.querySelector('.header__navbar__item')
 const headerButton = document.querySelector('.header__button');
 
+// Addition of event listener on click of hamburger to display nav
 hamburger.addEventListener('click', () => {
     console.log("I am a fine boy");
     closehamburger.style.display = "inline-block";
@@ -13,6 +16,7 @@ hamburger.addEventListener('click', () => {
     navbar.classList.replace('header__navbar', 'navbar');
 })
 
+// Addition of event listener on click of close button to remove nav
 closehamburger.addEventListener('click', () => {
     console.log("I am a fine girl");
     hamburger.style.display = "inline-block";
@@ -20,3 +24,27 @@ closehamburger.addEventListener('click', () => {
     navbar.classList.replace('navbar', 'header__navbar');
     header.style.cssText = "min-height: 0px;"
 })
+
+const testimonials = [document.querySelector('.testimonials__list__one'), 
+document.querySelector('.testimonials__list__two'), document.querySelector('.testimonials__list__three'), document.querySelector('.testimonials__list__four')]
+
+const circles = [document.querySelector('.testimonials__circle--one'), 
+document.querySelector('.testimonials__circle--two'), document.querySelector('.testimonials__circle--three'),
+document.querySelector('.testimonials__circle--four') 
+]
+circles[0].style.backgroundColor = "var(--primary-red)";
+
+for (const circle of circles){
+    circle.addEventListener('click', () => {
+        const uncheckCircle = circles.map((item) => item.style.backgroundColor = "var(--gray)");
+        const displayedTestimonial = testimonials.filter((item) => item.style.display = "block");
+        const disappearTestimonial = displayedTestimonial.map((item) => item.style.display = 'none');
+        circlePosition = circles.indexOf(circle);
+    
+        // On click, add a background-color to the one clicked
+        circle.style.backgroundColor = "var(--primary-red)";
+        // Connect circle to the respective testimonial
+        testimonials[circlePosition].style.display = "block";
+    });
+}
+
